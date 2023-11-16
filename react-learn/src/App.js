9e8c545b2c1ebcaa244b842b41d0ddaecf16d2cd
Svet <mursvyats@gmail.com>
2023-11-17 00:51:12 +0500

    Added input

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,19 +1,35 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import './styles/App.css';
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
     const [posts, setPosts] = useState([
         {id: 1, title: 'JavaScript', body: 'Description'},
         {id: 2, title: 'TypeScript', body: 'Description'},
         {id: 3, title: 'React', body: 'Description'}]);
+    const [title, setTitle] = useState('');
+    const bodyInputRef = useRef();
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        console.log(title);
+        console.log(bodyInputRef.current.value);
+    }
 
   return (
     <div className="App">
         <form>
-            <input type="text" placeholder="Post Description"/>
-            <input type="text" placeholder="Post Description"/>
-            <MyButton disabled>
+            <MyInput
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                type="text"
+                placeholder="Post Description"
+            />
+            <MyInput type="text" placeholder="Post Description"/>
+            <MyButton
+                onClick={addNewPost}
+            >
                 Create
             </MyButton>
         </form>
